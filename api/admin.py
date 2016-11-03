16a0d60403a080cec9dd2393e121c2413b138840
Ihor Gorobets <ihorgorobets@gmail.com>
2016-11-03 11:41:25 +0200

    Add models in adminpanel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,53 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import Country, Domain, Pub, Provider, PrList
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'country_name')
+admin.site.register(Country, CountryAdmin)
+
+
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ('id', 'domain', 'check_date')
+    list_filter = ('check_date',)
+admin.site.register(Domain, DomainAdmin)
+
+
+class PubAdmin(admin.ModelAdmin):
+    list_display = ('id', 'pub_id', 'source', 'domain')
+    list_filter = ('source', 'domain')
+admin.site.register(Pub, PubAdmin)
+
+
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'title', 'description', 'config')
+    search_fields = ('name',)
+admin.site.register(Provider, ProviderAdmin)
+
+
+class PrListAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'country',
+        'source',
+        'root_list',
+        'list_name',
+        'creation_date',
+        'modify_date',
+        'list_type',
+        'bid',
+        'list_id_on_src',
+        'archived',
+    )
+    list_filter = (
+        'country',
+        'source',
+        'root_list',
+        'creation_date',
+        'modify_date',
+        'archived',
+    )
+    raw_id_fields = ('pubs',)
+admin.site.register(PrList, PrListAdmin)
