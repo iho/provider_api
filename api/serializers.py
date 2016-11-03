f6eecbeb0d49c33db211b94d7b1fa93c420c897b
Ihor Gorobets <ihorgorobets@gmail.com>
2016-11-03 11:50:44 +0200

    Update to DRF 3.5.2

--- api/serializers.py
+++ api/serializers.py
@@ -6,27 +6,32 @@ class CountrySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Country
+        fields = '__all__'
 
 
 class DomainSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Domain
+        fields = '__all__'
 
 
 class PubSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Pub
+        fields = '__all__'
 
 
 class ProviderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Provider
+        fields = '__all__'
 
 
 class PrListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PrList
+        fields = '__all__'
